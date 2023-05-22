c73698b8bab62de164c65e6646e0713260dfa273
xiaodeaux <xiaodeaux@gmail.com>
2023-05-22 11:42:00 -0500

    change card name keys

--- src/TarotCard.tsx
+++ src/TarotCard.tsx
@@ -1,7 +1,12 @@
 import './TarotCard.css'
 
 const TarotCard = (props) => {
-  return <img src={props.url} className="tarot-card" />
+  return (
+    <>
+      <img src={props.url} className="tarot-card" />
+      <p></p>
+    </>
+  )
 }
 
 export default TarotCard
