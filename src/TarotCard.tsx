2693c8b2a0d412ac5e77c3d7e4e90cd85a3c3137
xiaodeaux <xiaodeaux@gmail.com>
2023-06-03 14:37:33 -0500

    have a random chance to reverse the tarot cards

--- src/TarotCard.tsx
+++ src/TarotCard.tsx
@@ -1,14 +1,38 @@
 import './TarotCard.css'
 
+let isReversed = false
+
+// create a function that will turn the image upside with a 50% chance to represent that the tarot card is reversed
+const randomRotation = () => {
+  const random = Math.random()
+  if (random < 0.5) {
+    isReversed = true
+    return 'rotate(180deg)'
+  } else {
+    isReversed = false
+    return 'rotate(0deg)'
+  }
+}
+
+// How do I use randomRotation
+// <img src={props.url} className="tarot-card" style={{ transform: randomRotation() }} />
+
 const TarotCard = (props) => {
   return (
     <>
       <div>
         {/* show the name of the card's function in the tarot spread */}
         <p className="tarot-function">{props.function}</p>
-        <img src={props.url} className="tarot-card" />
+        <img
+          src={props.url}
+          className="tarot-card"
+          style={{ transform: randomRotation() }}
+        />
         {/* show the name of the card under the image */}
-        <p className="tarot-name">{props.name}</p>
+        <p className="tarot-name">
+          {props.name}
+          {isReversed ? ' (Reversed)' : ''}
+        </p>
       </div>
     </>
   )
